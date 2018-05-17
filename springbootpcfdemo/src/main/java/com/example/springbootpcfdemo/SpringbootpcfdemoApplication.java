79d22f8976d0950d4aaca7220fa98f4442ccffa6
Nitin Gadgil <nitin_gadgil@ultimatesoftware.com>
2018-05-17 10:20:17 -0400

    Spring Boot PCF Demo

---
+++ springbootpcfdemo/src/main/java/com/example/springbootpcfdemo/SpringbootpcfdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootpcfdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpcfdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootpcfdemoApplication.class, args);
+    }
+}
